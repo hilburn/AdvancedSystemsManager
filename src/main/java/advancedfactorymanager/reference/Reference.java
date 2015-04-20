871fca57f83bc246e462d65cbbf919c168dd3bb3
hilburn <charliepaterson89@gmail.com>
2015-04-20 20:08:13 +0100

    Updated things

--- src/main/java/advancedfactorymanager/reference/Reference.java
+++ src/main/java/advancedfactorymanager/reference/Reference.java
@@ -4,10 +4,10 @@ public class Reference
 {
 
     // User friendly version of our mods name.
-    public static final String NAME = "Steve\'s Addons";
+    public static final String NAME = "Advanced Factory Manager";
 
     // Internal mod name used for reference purposes and resource gathering.
-    public static final String ID = "StevesAddons";
+    public static final String ID = "AdvancedFactoryManager";
 
     // Main version information that will be displayed in mod listing and for other purposes.
     public static final String V_MAJOR = "@MAJOR@";
