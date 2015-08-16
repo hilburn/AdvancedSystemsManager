27ebcf83d3c71c0f4fcaea7e1c70b458b66feace
hilburn <charliepaterson89@gmail.com>
2015-08-16 20:16:17 +0100

    Fixes loading order issue with WAILA

--- src/main/java/advancedsystemsmanager/compatibility/waila/WailaCompat.java
+++ src/main/java/advancedsystemsmanager/compatibility/waila/WailaCompat.java
@@ -14,10 +14,14 @@ import mcp.mobius.waila.api.IWailaRegistrar;
 import net.minecraft.inventory.IInventory;
 import net.minecraftforge.fluids.IFluidHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WailaCompat extends CompatBase
 {
     private static WailaLabelProvider labelProvider;
     private static Object registrar;
+    private static List<Class> labelClasses = new ArrayList<Class>();
 
     @Optional.Method(modid = Mods.WAILA)
     public static void callbackRegister(IWailaRegistrar registrar)
@@ -28,6 +32,10 @@ public class WailaCompat extends CompatBase
         registrar.registerHeadProvider(labelProvider, IFluidHandler.class);
         registrar.registerHeadProvider(labelProvider, TileEntityClusterElement.class);
         registrar.registerHeadProvider(labelProvider, TileEntityCluster.class);
+        for (Class clazz : labelClasses)
+        {
+            registrar.registerHeadProvider(labelProvider, clazz);
+        }
 
         registrar.registerBodyProvider(new ClusterDataProvider(), TileEntityCluster.class);
         registrar.registerBodyProvider(new RedstoneOutputDataProvider(), BlockCableOutput.class);
@@ -45,7 +53,13 @@ public class WailaCompat extends CompatBase
     {
         if (mod.isLoaded())
         {
-            ((IWailaRegistrar) registrar).registerHeadProvider(labelProvider, clazz);
+            if (registrar != null)
+            {
+                ((IWailaRegistrar) registrar).registerHeadProvider(labelProvider, clazz);
+            } else
+            {
+                labelClasses.add(clazz);
+            }
         }
     }
 }
